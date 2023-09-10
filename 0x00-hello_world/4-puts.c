c716b52520a989e6c5173366ff03b0cff7001868
vievieann <ifedibavivian12@gmail.com>
2023-09-09 17:48:23 -0700

    HH

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main-this is a put function
+ * main-this is the main put function
  * Return:0
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
