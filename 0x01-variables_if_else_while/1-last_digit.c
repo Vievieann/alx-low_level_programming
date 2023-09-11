a1fe6ef524ae71f837f438323ecdfb06986034be
vievieann <ifedibavivian12@gmail.com>
2023-09-11 15:36:32 -0700

    T1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -31,23 +31,4 @@ int main(void)
 
 
 
-Task 2
-
-#include <stdio.h>
-
-
-/**
- * main - program that prints the alphabet in lowercase
- *
- *  Return: Always 0.
- */
-int main(void)
-{
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-	putchar(letter);
-	putchar('\n');
-	return (0);
-}
 
