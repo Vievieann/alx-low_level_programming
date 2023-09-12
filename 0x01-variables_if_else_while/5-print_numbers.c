fd0802c77836898c1a8bd5cb7eb943d37f2e4680
vievieann <ifedibavivian12@gmail.com>
2023-09-11 17:29:38 -0700

    T5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
-
 /**
- * main - program that prints the alphabet in lowercase and upper case
- *
- * Return: Always 0.
- */
+* main - program that prints the alphabet in lowercase and upper case
+*
+* Return: Always 0.
+*/
 
 int main(void)
 {
-	char letter;
+char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+for (letter = 'a'; letter <= 'z'; letter++)
 
-	{
-	if (letter != 'e' && letter != 'q')
-	putchar(letter);
-	}
+{
+if (letter != 'e' && letter != 'q')
+putchar(letter);
+}
 
-	putchar('\n');
+putchar('\n');
 
-	return (0);
+return (0);
 }
 
